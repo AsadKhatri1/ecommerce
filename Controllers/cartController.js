52d907f8ff0b97b95a10ad3309dd225ee3e3bb9b
Asad khatri <92842688+AsadKhatri1@users.noreply.github.com>
2024-07-23 17:53:27 +0500

    tillcartapis

--- Controllers/cartController.js
+++ Controllers/cartController.js
@@ -1,9 +1,10 @@
 import { cartModel } from "../Models/cartModel.js";
 
+// add to cart
 export const addToCart = async (req, res) => {
   try {
     const { productId, title, qty, price, imgSrc } = req.body;
-    const userId = "669e311db038ed4eb28e41f6";
+    const userId = "669e282d13b5f191628c5938";
     let cart = await cartModel.findOne({ userId });
     if (!cart) {
       cart = new cartModel({ userId, items: [] });
@@ -23,4 +24,91 @@ export const addToCart = async (req, res) => {
   } catch (err) {
     res.json({ message: err.message });
   }
+};
+
+// user specific cart
+export const userCart = async (req, res) => {
+  try {
+    const userId = "669e311db038ed4eb28e41f6";
+    let cart = await cartModel.findOne({ userId });
+    if (!cart) return res.json({ message: "Cart not found with this user id" });
+    return res.json({ message: "user cart", cart });
+  } catch (err) {
+    res.json({ message: err.message });
+  }
+};
+
+// delete specific product from cart
+export const removeProduct = async (req, res) => {
+  try {
+    const userId = "669e311db038ed4eb28e41f6";
+    const productId = req.params.productId;
+    let cart = await cartModel.findOne({ userId });
+    if (!cart) return res.json({ message: "Cart not found with this user id" });
+
+    // removing using filters
+    cart.items = cart.items.filter(
+      (item) => item.productId.toString() !== productId
+    );
+    await cart.save();
+    return res.json({ message: "Product removed from cart", cart: cart.items });
+  } catch (err) {
+    res.json({ message: err.message });
+  }
+};
+
+// cart clearing
+
+export const clearCart = async (req, res) => {
+  try {
+    const userId = "669e311db038ed4eb28e41f6";
+
+    let cart = await cartModel.findOne({ userId });
+    if (!cart) {
+      cart = new cart({ userId, items: [] });
+    } else {
+      cart.items = [];
+    }
+
+    await cart.save();
+    return res.json({ message: "cart is now cleared", cart });
+  } catch (err) {
+    res.json({ message: err.message });
+  }
+};
+
+// decrease qty from Cart
+export const decreaseProudctQty = async (req, res) => {
+  const { productId, qty } = req.body;
+
+  const userId = "669e311db038ed4eb28e41f6";
+
+  let cart = await cartModel.findOne({ userId });
+
+  if (!cart) {
+    cart = new cartModel({ userId, items: [] });
+    // return res.json({messge:'Cart not find'})
+  }
+
+  const itemIndex = cart.items.findIndex(
+    (item) => item.productId.toString() === productId
+  );
+
+  if (itemIndex > -1) {
+    const item = cart.items[itemIndex];
+
+    if (item.qty > qty) {
+      const pricePerUnit = item.price / item.qty;
+
+      item.qty -= qty;
+      item.price -= pricePerUnit * qty;
+    } else {
+      cart.items.splice(itemIndex, 1);
+    }
+  } else {
+    return res.json({ messge: "invalid product Id" });
+  }
+
+  await cart.save();
+  res.json({ message: "Items qty decreased", cart });
 };
--- Routes/cart.js
+++ Routes/cart.js
@@ -1,9 +1,28 @@
 import express from "express";
-import { addToCart } from "../Controllers/cartController.js";
+import {
+  addToCart,
+  clearCart,
+  decreaseProudctQty,
+  removeProduct,
+  userCart,
+} from "../Controllers/cartController.js";
 
 const router = express.Router();
 
 // add to cart
 router.post("/add", addToCart);
 
+// get specific user route
+
+router.get("/usercart", userCart);
+
+// remove specific product from cart
+
+router.delete("/remove/:productId", removeProduct);
+// clear cart
+
+router.delete("/clear", clearCart);
+// decrease item qty
+
+router.post("/decreaseQty", decreaseProudctQty);
 export default router;
